e56aef2953e318cd7814bcbe5813162a416d6df0
Soufian <soufian.elbakkali@hotmail.com>
2023-01-12 18:03:26 +0100

    React + OpenLayers AIS Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import 'ol/ol.css';
+import {useGeographic} from "ol/proj";
+import MyMap from "./MyMap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const geo = useGeographic()
+    return (
+        <div>
+            <MyMap />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/MyMap.js
@@ -0,0 +1,133 @@
+import React, {Component, useState} from "react";
+import Map from "ol/Map";
+import View from "ol/View";
+import LayerTile from "ol/layer/Tile";
+import SourceOSM from "ol/source/OSM";
+import Vector from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import GeoJSON from "ol/format/GeoJSON"
+import Style from "ol/style/Style"
+import Stroke from "ol/style/Stroke"
+import Circle from "ol/style/Circle"
+import Fill from "ol/style/Fill"
+import MultiPoint from "ol/geom/MultiPoint"
+import {getVectorContext} from 'ol/render';
+
+class PublicMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {center: [4, 50], zoom: 5, tmp: 0};
+
+        this.map = new Map({
+            target: null,
+            layers: [
+                new LayerTile({
+                    source: new SourceOSM()
+                })
+            ],
+            view: new View({
+                center: this.state.center,
+                zoom: this.state.zoom
+            })
+        });
+
+        this.getData();
+        this.map.render();
+    }
+
+
+    getData() {
+        var file = 'https://raw.githubusercontent.com/SoufianBk/MobilityDB-OL/main/trips.json';
+
+        var SRC_bigJSON = new Vector({
+            url: file,  // big JSON file
+            format: new GeoJSON()
+        });
+
+        var bigJSON = new VectorLayer({
+            source: SRC_bigJSON,
+            style: new Style({
+                stroke: new Stroke({
+                    color: 'rgba(255, 255, 255, 0)'
+                })
+            })
+        });
+        this.map.addLayer(bigJSON);
+
+        const ptStyle = new Style({
+            image: new Circle({
+                radius: 3,
+                fill: new Fill({color: 'yellow'}),
+                stroke: new Stroke({color: 'red', width: 1}),
+            }),
+        });
+
+        this.bigJSONPostRender(bigJSON, ptStyle, SRC_bigJSON)
+    }
+
+    bigJSONPostRender(bigJSON, ptStyle, SRC_bigJSON) {
+        var features
+        var tmp = 0
+        var map2 = this.map
+        bigJSON.on('postrender', function (event) {
+            const vectorContext = getVectorContext(event);
+            tmp = tmp + 1
+
+            features = SRC_bigJSON.getFeatures();
+            var first = features[1200]
+
+            var coordinates = []
+
+            features.forEach((feature) => {
+                if (feature.getGeometry().getCoordinates().length > tmp) {
+                    coordinates.push(feature.getGeometry().getCoordinates()[tmp])
+                }
+            });
+
+            console.info(first.getGeometry().getCoordinates().length)
+
+            vectorContext.setStyle(ptStyle);
+            vectorContext.drawGeometry(new MultiPoint(coordinates));
+
+            if (tmp < 1200) {
+                map2.render()
+            }
+        });
+    }
+
+    updateMap() {
+        this.map.getView().setCenter(this.state.center);
+        this.map.getView().setZoom(this.state.zoom);
+    }
+
+    componentDidMount() {
+        this.map.setTarget("map");
+
+        // Listen to map changes
+        this.map.on("moveend", () => {
+            let center = this.map.getView().getCenter();
+            let zoom = this.map.getView().getZoom();
+            this.setState({center, zoom});
+        });
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        let center = this.map.getView().getCenter();
+        let zoom = this.map.getView().getZoom();
+        if (center === nextState.center && zoom === nextState.zoom) return false;
+        return true;
+    }
+
+    userAction() {
+        this.setState({center: [546000, 6868000], zoom: 5});
+    }
+
+    render() {
+        return (
+            <div id="map" style={{width: "100%", height: "1050px"}}></div>
+        );
+    }
+}
+
+export default PublicMap;
